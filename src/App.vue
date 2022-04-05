abcaca5452c655605c694396fd8ffa53c1f322b3
Luth-DWN <luth.dwn@gmail.com>
2022-04-06 03:26:42 +0700

    update task

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,6 @@
       <MainContent :articles="articles"></MainContent> <!-- Add the component in the template -->
     </v-container>
    </v-content>
-   <v-footer class="secondary" app>
-      <v-layout row wrap align-center>
-        <v-flex xs12>
-          <div class="white--text ml-3">
-            Made with
-            <v-icon class="red--text">favorite</v-icon>
-            by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-            and <a class="white--text" href="https://github.com/rachidsakara" 
-target="_blank">Rachid Sakara</a>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-footer>
   </v-app>
 
 
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,56 +1,82 @@
 <template>
-    <v-layout row wrap align-center>
-          <v-flex xs8  offset-md2>
-            <div v-for="article in articles" :key="article.title">
-              <v-card class="my-3" hover>
-                <v-img
-                  height="350px"
-                  v-bind:src="article.urlToImage"
-                ></v-img>
-                  <v-container fill-height fluid>
-                    <v-layout>
-                      <v-flex xs12 align-end d-flex>
-                        <span class="headline">{{ article.title }}</span>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                <v-card-text>
-                  {{ article.description }}
-                </v-card-text>
-                <v-card-actions>
-                  <v-chip small color="secondary" class="white--text">
-                    {{article.source.name}}
-                  </v-chip>
-                  <v-spacer></v-spacer>
-                  <v-btn icon class="red--text">
-                    <v-icon small>fa-reddit</v-icon>
-                  </v-btn>
-                  <v-btn icon class="light-blue--text">
-                    <v-icon small>fa-twitter</v-icon>
-                  </v-btn>
-                  <v-btn icon class="blue--text text--darken-4">
-                    <v-icon small>fa-facebook</v-icon>
-                  </v-btn>
-                  <v-btn icon class="red--text">
-                    <v-icon small>fa-google-plus</v-icon>
-                  </v-btn>
-                  <v-btn icon class="blue--text text--darken-4">
-                    <v-icon small>fa-linkedin</v-icon>
-                  </v-btn>
-                  <v-spacer></v-spacer>
 
-         <v-btn small replace color="info" v-bind:href="article.url" target="_blank" >Read More</v-btn>
-                </v-card-actions>
-              </v-card>
-            </div>
-          </v-flex>
-        </v-layout>
+  <v-layout row wrap align-center>
+    <v-navigation-drawer
+      absolute
+      permanent
+      left
+    >
+
+      <v-list dense>
+        <v-list-item
+          v-for="article in articles"
+          :key="article.title"
+        >
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/lego/1.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ article.author }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-flex xs8 offset-md2>
+      <div v-for="article in articles" :key="article.title">
+        <v-card class="my-3" hover>
+          <v-img height="350px" v-bind:src="article.urlToImage"></v-img>
+          <v-container fill-height fluid>
+            <v-layout>
+              <v-flex xs12 align-end d-flex>
+                <span class="headline">{{ article.title }}</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-card-text>
+            {{ article.description }}
+          </v-card-text>
+          <v-card-actions>
+            <v-chip small color="secondary" class="white--text">
+              {{ article.source.name }}
+            </v-chip>
+            <v-spacer></v-spacer>
+            <v-btn icon class="red--text">
+              <v-icon small>fa-reddit</v-icon>
+            </v-btn>
+            <v-btn icon class="light-blue--text">
+              <v-icon small>fa-twitter</v-icon>
+            </v-btn>
+            <v-btn icon class="blue--text text--darken-4">
+              <v-icon small>fa-facebook</v-icon>
+            </v-btn>
+            <v-btn icon class="red--text">
+              <v-icon small>fa-google-plus</v-icon>
+            </v-btn>
+            <v-btn icon class="blue--text text--darken-4">
+              <v-icon small>fa-linkedin</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              small
+              replace
+              color="info"
+              v-bind:href="article.url"
+              target="_blank"
+              >Read More</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
   props: {
-      articles: Array
+    articles: Array,
   },
-}
+};
 </script>
