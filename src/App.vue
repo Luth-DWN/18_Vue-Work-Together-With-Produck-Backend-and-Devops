df5280737343fba2aec714e432e38aed281fec5f
Luth-DWN <luth.dwn@gmail.com>
2022-04-06 03:28:23 +0700

    asdasdasd

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
 <v-app light>
-  <!--The SideMenu Component go here-->  
   <v-toolbar fixed app light clipped-left color="primary" class="elevation-2">
     <v-toolbar-side-icon @click="drawer = !drawer"  class="white--text"></v-toolbar-side-icon>
     <v-toolbar-title class="white--text">News App</v-toolbar-title>
@@ -8,7 +7,7 @@
 
   <v-content>
     <v-container fluid>
-      <MainContent :articles="articles"></MainContent> <!-- Add the component in the template -->
+      <MainContent :articles="articles"></MainContent> 
     </v-container>
    </v-content>
   </v-app>
@@ -18,19 +17,19 @@
 
 <script>
 
-import axios from 'axios' // importing the axios (a HTTP library) to connects the app with the News API
-import MainContent from './components/MainContent.vue' // import the Main Content component
+import axios from 'axios'
+import MainContent from './components/MainContent.vue'
 
 export default {
 
   components: {
-        MainContent, // Register the component
+        MainContent,
   },
 
   data() {
     return {
-      drawer: false, // false = Vuetify automatically "do the right thing" to show/hide the drawer
-      api_key:'9e704ef41bb048afb46a851b69d62a28', // Your API Key go here
+      drawer: false,
+      api_key:'9e704ef41bb048afb46a851b69d62a28',
       articles: [],
       errors: [] 
     }
@@ -38,10 +37,9 @@ export default {
   created () {
     axios.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key)
       .then(response => {
-        //this.articles = response.data.articles
         this.articles = response.data.articles
         console.log('data:')
-        console.log(response.data.articles) // This will give you access to the full object
+        console.log(response.data.articles)
       })
       .catch(e => {
         this.errors.push(e)
